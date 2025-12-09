162329d321d896022ddc205a78bac13116c41e7e
ahmed-1430 <ahmedtexting@gmail.com>
2025-12-09 23:42:27 +0600

    Basic Routes Created on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import ProtectedRoute from './components/ProtectedRoute';
 
-function App() {
-  const [count, setCount] = useState(0)
 
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Routes>
+      <Route path='/' element={<h1>Home</h1>} />
+      <Route path='/login' element={<h1>Login</h1>} />
+      <Route path='/register' element={<h1>Register</h1>} />
 
-export default App
+
+      <Route element={<ProtectedRoute />}>
+        <Route path='/dashboard' element={<h1>dashboard Layout</h1>} />
+      </Route>
+
+
+      {/* Example admin-only nested route */}
+      <Route element={<ProtectedRoute roles={["admin"]} />}>
+        <Route path='/admin' element={<div>Admin area</div>} />
+      </Route>
+    </Routes>
+  );
+}
